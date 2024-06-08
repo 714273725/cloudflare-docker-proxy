b54d2589e506697a284cf6587037afdb6e04ddba
714273725 <714276725@qq.com>
2024-06-08 14:54:39 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,9 @@ addEventListener("fetch", (event) => {
 });
 
 const routes = {
-  "docker.libcuda.so": "https://registry-1.docker.io",
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker-mirror-registry.outside.xiaotuizi.top": "https://registry-1.docker.io",
+  "docker-mirror-quay.outside.xiaotuizi.top": "https://quay.io",
+  "docker-mirror-gcr.outside.xiaotuizi.top": "https://gcr.io",
 };
 
 function routeByHosts(host) {
